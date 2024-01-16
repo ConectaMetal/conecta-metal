1d603a69bbd2f764ef6aea7c5d9f81e0f9ad62ef
Kauê Tribess <kaueirontribess@gmail.com>
2024-01-16 17:05:26 -0300

    Made migrations in the database

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Companies
 
 # Register your models here.
+@admin.register(Companies)
+class CompaniesAdmin(admin.ModelAdmin):
+    ...
--- main_app/models.py
+++ main_app/models.py
@@ -2,10 +2,37 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class Empresas(models.Model):
+class Companies(models.Model):
     CNPJ = models.CharField(max_length=14)
-    razaoSocial = models.CharField(max_length=255)
-    nomeFantasia = models.CharField(max_length=255)
+    legalName = models.CharField(max_length=255)
+    businessName = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255)
     profilePic = models.ImageField(upload_to='main/prof_pics/%Y/%m/%d/')
+    registrationStatus = models.CharField(max_length=100)
+    CEP = models.CharField(max_length=10)
+    address = models.CharField(max_length=255)
+    phoneNumber = models.CharField(max_length=20, blank=True)
+    email = models.CharField(max_length=255)
+    rating = models.FloatField()
+    validatingDocument = models.FileField()
+    isPartner = models.BooleanField(default=False)
     isValid = models.BooleanField(default=False)
 
+    def __str__(self):
+        return self.legalName
+
+
+class SocialMedia(models.Model):
+    ...
+
+
+class Products(models.Model):
+    ...
+
+
+class Services(models.Model):
+    ...
+
+
+class Invoice(models.Model):
+    ...
