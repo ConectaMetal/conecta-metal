7fbb40c7d856a9454a4fa5073068426c6bd8d207
Kauê Tribess <kaueirontribess@gmail.com>
2024-01-18 19:55:43 -0300

    Make product.html use the database information

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,5 +5,5 @@ app_name = 'main_app'
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('product/', views.product, name="product")
+    path('product/<slug:slug>', views.product, name="product")
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,12 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from main_app.models import Products
+from utils.main.rating import product_rating
 
 # Create your views here.
 def home(request):
     products = Products.objects.all().order_by('-id')
-    context = {'products': products}
+    context = {'products': products, 'product_rating': product_rating}
     return render(request=request, template_name='main/pages/home.html', context=context)
 
 
-def product(request):
-    return render(request=request, template_name='main/pages/product.html')
+def product(request, slug):
+    product = get_object_or_404(
+        Products, slug=slug
+    )
+    context = {'product': product}
+
+    return render(request=request, template_name='main/pages/product.html', context=context)
